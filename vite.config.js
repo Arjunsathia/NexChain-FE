35d4421925a99a25406b08264cbede13e6ea875f
Arjun <arjunsathia123@gmail.com>
2025-12-09 11:42:57 +0530

    fix build

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,32 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "vendor-react": ["react", "react-dom", "react-router-dom"],
+          "vendor-apexcharts": ["apexcharts", "react-apexcharts"],
+          "vendor-chartjs": ["chart.js", "react-chartjs-2"],
+          "vendor-recharts": ["recharts"],
+          "vendor-ui": [
+            "framer-motion", 
+            "lottie-react", 
+            "react-confetti", 
+            "react-loading-skeleton",
+            "react-simple-typewriter"
+          ],
+          "vendor-utils": [
+             "axios", 
+             "zod", 
+             "react-hook-form", 
+             "@tanstack/react-query",
+             "@stripe/stripe-js"
+          ],
+          "vendor-icons": ["react-icons", "lucide-react"],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
 });
