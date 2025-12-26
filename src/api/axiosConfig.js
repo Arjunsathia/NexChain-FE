cb83ef8f1a6b20912a2aa0ea60b62e8c90e6b9bd
arjun <arjunsathia123@gmail.com>
2025-12-26 16:32:37 +0530

    debug

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:5050/api";
+const API_BASE_URL = import.meta.env.VITE_BASE_URL || `http://${window.location.hostname}:5050/api`;
 
 const COINGECKO_BASE_URL = import.meta.env.DEV 
   ? "/api/coingecko" 
